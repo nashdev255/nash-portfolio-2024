3ec58e29ffa33d4627b7c621fc5cc51dc481227c
nashdev255 <nashdev255@gmail.com>
2024-02-28 21:28:37 +0900

    Small ajustment

--- src/app/components/Headline.tsx
+++ src/app/components/Headline.tsx
@@ -3,7 +3,7 @@ const Headline = () => {
     <div className='my-12 flex min-h-[60vh] items-center justify-center'>
       <div className='flex flex-col md:flex-row md:space-x-12'>
         <div className='mx-auto my-6 size-[25vh] rounded-full bg-gradient-to-bl from-blue-500 via-green-600 to-yellow-300' />
-        <div className='my-0 md:my-auto'>
+        <div className='my-0 space-y-2 md:my-auto'>
           <h1 className='text-4xl font-bold'>Nash</h1>
           <p className='text-2xl'>Digital Content Developer</p>
         </div>
