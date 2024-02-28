4178b52bae4ac03888243631fbc9064601482960
nashdev255 <nashdev255@gmail.com>
2024-02-28 23:36:37 +0900

    Adjust height

--- src/app/components/Headline.tsx
+++ src/app/components/Headline.tsx
@@ -1,6 +1,6 @@
 const Headline = () => {
   return (
-    <div className='my-12 flex min-h-[64vh] items-center justify-center'>
+    <div className='my-12 flex min-h-[80vh] items-center justify-center'>
       <div className='flex flex-col md:flex-row md:space-x-12'>
         <div className='mx-auto my-6 size-[25vh] rounded-full bg-gradient-to-bl from-blue-500 via-green-600 to-yellow-300' />
         <div className='my-0 space-y-2 md:my-auto'>
