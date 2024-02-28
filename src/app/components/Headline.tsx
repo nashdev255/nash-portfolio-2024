ed7d8bf95ec7fd9f5430ff98ec2872a86a84289b
nashdev255 <nashdev255@gmail.com>
2024-02-28 21:26:58 +0900

    Update Headline

--- src/app/components/Headline.tsx
+++ src/app/components/Headline.tsx
@@ -1,11 +1,11 @@
 const Headline = () => {
   return (
-    <div className='my-12 flex justify-center'>
-      <div className='flex flex-col md:flex-row md:space-x-8'>
-        <div className='mx-auto my-6 size-[25vh] rounded-full bg-gradient-to-bl from-blue-500 via-green-600 to-yellow-300 md:m-0' />
-        <div className='my-auto'>
-          <h1 className='text-3xl font-bold'>Nash</h1>
-          <p className='text-xl'>Digital Content Developer</p>
+    <div className='my-12 flex min-h-[60vh] items-center justify-center'>
+      <div className='flex flex-col md:flex-row md:space-x-12'>
+        <div className='mx-auto my-6 size-[25vh] rounded-full bg-gradient-to-bl from-blue-500 via-green-600 to-yellow-300' />
+        <div className='my-0 md:my-auto'>
+          <h1 className='text-4xl font-bold'>Nash</h1>
+          <p className='text-2xl'>Digital Content Developer</p>
         </div>
       </div>
     </div>
