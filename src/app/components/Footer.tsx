704d501a5d0c2da6939f7311169c3c523050dd2f
nashdev255 <nashdev255@gmail.com>
2024-03-02 19:25:46 +0900

    change footer's flex direction for mobile

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className='flex h-[10vh] justify-center'>
-      <div className='flex h-full min-w-[75vw] items-center justify-between px-4'>
+    <footer className='flex justify-center'>
+      <div className='my-8 flex h-full min-w-[75vw] flex-col items-center justify-between space-y-4 px-4 md:flex-row md:space-y-0'>
         <span>{ new Date().getFullYear() } &copy; All Rights Reserved</span>
         <p>
           <Link href='https://github.com/nashdev255'>nashdev255</Link>
