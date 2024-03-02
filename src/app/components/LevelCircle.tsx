e54b98f0913814968dab962dd535da13e779e217
nashdev255 <nashdev255@gmail.com>
2024-03-02 12:14:39 +0900

    Add Status

--- src/app/components/LevelCircle.tsx
+++ src/app/components/LevelCircle.tsx
@@ -5,7 +5,7 @@ const LevelCircle = ({ level, textStyle, backgroundStyle }: {
 }) => {
   const translateFromTop = 100 - level;
   return (
-    <div className='border-theme relative size-32 overflow-hidden rounded-full md:size-36'>
+    <div className='border-theme relative size-32 overflow-hidden rounded-full md:size-36 lg:size-40'>
       <h3 className={`absolute left-1/2 top-1/2 z-10 translate-x-[-50%] translate-y-[-50%] text-3xl font-bold ${textStyle}`}>{level}%</h3>
       <div style={{ transform: `translateY(${translateFromTop}%)` }} className='size-full bg-gradient-to-bl from-red-700 via-orange-400 to-yellow-300' />
     </div>
--- src/app/components/Status.tsx
+++ src/app/components/Status.tsx
@@ -29,34 +29,13 @@ const Status = () => {
         </li>
         <li className='flex flex-col space-y-4'>
           <div className='flex justify-center'>
-            <LevelCircle level={80} textStyle='' backgroundStyle='' />
+            <LevelCircle level={40} textStyle='' backgroundStyle='' />
           </div>
           <div className='flex flex-col items-center space-y-4'>
-            <h3 className='text-lg font-bold'>言語能力</h3>
+            <h3 className='text-lg font-bold'>コミュニケーション能力</h3>
             <p>
-              英語をはじめとして、中国語、韓国語を扱う能力に長けている。
-            </p>
-          </div>
-        </li>
-        <li className='flex flex-col space-y-4'>
-          <div className='flex justify-center'>
-            <LevelCircle level={80} textStyle='' backgroundStyle='' />
-          </div>
-          <div className='flex flex-col items-center space-y-4'>
-            <h3 className='text-lg font-bold'>言語能力</h3>
-            <p>
-              英語をはじめとして、中国語、韓国語を扱う能力に長けている。
-            </p>
-          </div>
-        </li>
-        <li className='flex flex-col space-y-4'>
-          <div className='flex justify-center'>
-            <LevelCircle level={80} textStyle='' backgroundStyle='' />
-          </div>
-          <div className='flex flex-col items-center space-y-4'>
-            <h3 className='text-lg font-bold'>言語能力</h3>
-            <p>
-              英語をはじめとして、中国語、韓国語を扱う能力に長けている。
+              挨拶や礼儀といった基本的なコミュニケーション能力をとることができ、
+              かつ積極的に人に話しかけることができるが、言語化能力が低いため会話は苦手。
             </p>
           </div>
         </li>
@@ -68,6 +47,28 @@ const Status = () => {
             <h3 className='text-lg font-bold'>計画性</h3>
             <p>
               計画には忠実な反面、計画自体を立てることはできない。技術的負債を抱える可能性がある。
+            </p>
+          </div>
+        </li>
+        <li className='flex flex-col space-y-4'>
+          <div className='flex justify-center'>
+            <LevelCircle level={60} textStyle='' backgroundStyle='' />
+          </div>
+          <div className='flex flex-col items-center space-y-4'>
+            <h3 className='text-lg font-bold'>経済に対する関心</h3>
+            <p>
+              世界的な経済動向や資産に興味を持っている。
+            </p>
+          </div>
+        </li>
+        <li className='flex flex-col space-y-4'>
+          <div className='flex justify-center'>
+            <LevelCircle level={10} textStyle='' backgroundStyle='' />
+          </div>
+          <div className='flex flex-col items-center space-y-4'>
+            <h3 className='text-lg font-bold'>政治に対する関心</h3>
+            <p>
+              歴史的な政治について面白いとは思うものの、現代社会の政治に対する関心は薄い。
             </p>
           </div>
         </li>
