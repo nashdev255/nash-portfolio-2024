62a3660ad329684959abaf844452b712a40a90fb
nashdev255 <nashdev255@gmail.com>
2024-03-04 18:01:21 +0900

    fix? issue

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -4,7 +4,7 @@ import Status from '@/app/components/Status';
 const Skills = () => {
   return (
     <div className='my-12 flex justify-center'>
-      <div className='max-w-[75vw]'>
+      <div className='max-w-[80vw] md:max-w-[75vw]'>
         <div className='my-6 flex h-[12vh] items-center justify-center'>
           <h1 className='text-4xl font-bold'>Skills</h1>
         </div>
