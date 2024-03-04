86d90337a6c8307576babd860799960bd834e109
nashdev255 <nashdev255@gmail.com>
2024-03-05 03:12:59 +0900

    Works add item itky-quest

--- src/app/components/Works.tsx
+++ src/app/components/Works.tsx
@@ -7,11 +7,11 @@ const Works = () => {
         </div>
         <ul className='grid w-full grid-cols-1 gap-24 lg:grid-cols-2 lg:gap-32'>
           <li className="relative flex items-center justify-center shadow-xl duration-75 hover:scale-105">
-            <div className="h-[30vh] w-[75vw] rounded-lg bg-slate-600 bg-[url('/studysynchub_header.png')] bg-cover bg-right md:h-[45vh] md:w-[70vw]" />
+            <div className="h-[30vh] w-[75vw] rounded-lg bg-[url('/works_headers/studysynchub.png')] bg-cover bg-right md:h-[45vh] md:w-[70vw]" />
             <h2 className='text-theme-dark absolute bottom-4 left-4 text-2xl font-bold md:bottom-6 md:left-6 md:text-3xl'>Study Sync Hub</h2>
           </li>
-          <li className='relative flex items-center justify-center shadow-xl duration-75 hover:scale-105'>
-            <div className='h-[30vh] w-[75vw] rounded-lg bg-slate-600 md:h-[45vh] md:w-[70vw]' />
+          <li className="relative flex items-center justify-center shadow-xl duration-75 hover:scale-105">
+            <div className="h-[30vh] w-[75vw] rounded-lg bg-slate-400 bg-[url('/works_headers/itky-quest.png')] bg-center md:h-[45vh] md:w-[70vw]" />
             <h2 className='text-theme-dark absolute bottom-4 left-4 text-2xl font-bold md:bottom-6 md:left-6 md:text-3xl'>イトキヨクエスト</h2>
           </li>
         </ul>
